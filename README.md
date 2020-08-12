59ae1c61e856dc65b3c5cd3f1c28b5090211b64c
berkiyo <berk@tekbyte.net>
2020-08-12 13:11:04 +1000

    Few updates, some boilerplate stuff added. - Action menu added - Colour scheme implemented - Bottom bar buttons - About popup implemented.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # apricot
-Air Quality + Temperature/Humidity App (EEET2609 Project)
+Air Quality + Temperature/Humidity App (EEET2609 Project) (Team9)
+
+Apricot is a meteorology and air quality system designed to be easy to use and stay out of your way.
+
+This was created as a group project (Team9) for the RMIT Engineering Design 3 project.
+
+# development
+This app is currently work-in-progress and is under heavy development. No pull-requests will be accepted for the time
+being because this is a university assignment. Once completed, if you wish to contribute, feel free to do so. (this disclaimer will disappear).
+However, feel free to open bug-reports by clicking on the "Issues" tab.
+
+
+Version v1.0.0
--- app/src/main/java/com/berkd/apricot/MainActivity.java
+++ app/src/main/java/com/berkd/apricot/MainActivity.java
@@ -1,8 +1,18 @@
 package com.berkd.apricot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,77 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        aboutPopup();
+    }
+
+
+    /**
+     * ESSENTIAL FOR MENU CREATION!
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_menu, menu);
+        return true;
+    }
+
+    /** /////////////////////// M E N U ////////////////////////
+     * WHEN OPTIONS MENU IS SELECT, CURRENTLY DOES NOT MUCH BUT WIP
+     * TODO -> Get these functions things implemented (CURRENTLY WIP)
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            // item1 = Clear List
+            // Standalone. Just does one thing.
+            case R.id.item1:
+                Toast.makeText(this, "Hello there.", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.item2:
+                //settingsPopup();
+                break;
+            case R.id.item3:
+                Toast.makeText(this, "Dark mode coming soon!", Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.item4:
+                aboutPopup();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * ABOUT POPUP
+     *  About the program and whatnot.
+     */
+    public void aboutPopup() {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.about_popup, null);
+        mBuilder.setTitle("About");
+
+        mBuilder.setNeutralButton("PROJECT PAGE", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Uri projectPage = Uri.parse("https://www.github.com/berkiyo/apricot");
+                Intent intent = new Intent(Intent.ACTION_VIEW, projectPage);
+                startActivity(intent);
+            }
+        });
+
+        mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        mBuilder.setView(mView);
+        AlertDialog dialog = mBuilder.create();
+        dialog.show();
     }
 }
