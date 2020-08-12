77d52a774d0b62aa292b850ada5aadc71b06594b
berkiyo <berk@tekbyte.net>
2020-08-12 13:55:40 +1000

    v1.0.0 - Basics implemented, lot's of features planned - Action menu added - Colour scheme implemented - Bottom bar buttons - About popup implemented.

--- app/src/main/java/com/berkd/apricot/MainActivity.java
+++ app/src/main/java/com/berkd/apricot/MainActivity.java
@@ -6,21 +6,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+    String[] units = {"Celsius", "Fahrenheit"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         aboutPopup();
+        buttonConnection();
+        buttonViewLogs();
+        buttonStartLogging();
     }
 
 
@@ -49,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
             case R.id.item2:
-                //settingsPopup();
+                settingsPopup();
                 break;
             case R.id.item3:
                 Toast.makeText(this, "Dark mode coming soon!", Toast.LENGTH_SHORT).show();
@@ -93,4 +105,119 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog dialog = mBuilder.create();
         dialog.show();
     }
+
+
+    /**
+     * SETTINGS POPUP
+     */
+    public void settingsPopup() {
+        final int unit = 0; // 0 == Celsius
+                            // 1 == Fahrenheit
+
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.settings_popup, null);
+        mBuilder.setTitle("Settings");
+
+        final Spinner mSpinner = mView.findViewById(R.id.unit_spinner);
+
+        final ArrayAdapter<String> sAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.units));
+
+        sAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(sAdapter);
+
+        // Load the previously saved font selection!
+        mSpinner.setSelection(loadSpinnerState());
+        //
+
+        mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                if(mSpinner.getSelectedItem().toString().equalsIgnoreCase("celsius")) {
+                    Toast.makeText(MainActivity.this, "Celsius set", Toast.LENGTH_SHORT).show();
+                    saveSpinnerState(0);
+                    dialog.dismiss();
+                }
+                if(mSpinner.getSelectedItem().toString().equalsIgnoreCase("fahrenheit")) {
+                    Toast.makeText(MainActivity.this, "Fahrenheit Set", Toast.LENGTH_SHORT).show();
+                    saveSpinnerState(1);
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        mBuilder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        mBuilder.setView(mView);
+        AlertDialog dialog = mBuilder.create();
+        dialog.show();
+    }
+
+
+    /**
+     * SAVE_SPINNER_STATE
+     *  Store the position of the spinner
+     */
+    public void saveSpinnerState(int userChoice) {
+        SharedPreferences sharedPref = getSharedPreferences("FileName",0);
+        SharedPreferences.Editor prefEditor = sharedPref.edit();
+        prefEditor.putInt("userChoiceSpinner", userChoice);
+        prefEditor.commit();
+    }
+
+    /**
+     * LOAD_SPINNER_STATE
+     *  Return the value of the previously selected font size, return integer array value
+     */
+    public int loadSpinnerState() {
+        SharedPreferences sharedPref = getSharedPreferences("FileName",MODE_PRIVATE);
+        int spinnerValue = sharedPref.getInt("userChoiceSpinner",-1);
+        return spinnerValue;
+    }
+
+    private void buttonConnection() {
+        Button buttonInsert = findViewById(R.id.buttonConnection);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Clicked! Functionality coming soon.", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+
+    private void buttonViewLogs() {
+        Button buttonInsert = findViewById(R.id.buttonLogs);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Clicked! Functionality coming soon.", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void buttonStartLogging() {
+        Button buttonInsert = findViewById(R.id.buttonStartLogging);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Clicked! Functionality coming soon.", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
 }
